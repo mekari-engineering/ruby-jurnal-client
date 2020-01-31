c03e76d49787fcb97c561e20a7ffadcc22f341f8
Sleekr <sleekr@Sleekrs-MacBook-Pro.local>
2020-01-31 17:26:14 +0700

    initial commit

---
+++ lib/jurnal/api.rb
@@ -0,0 +1,8 @@
+require "jurnal/api/version"
+
+module Jurnal
+  module Api
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/jurnal/api/version.rb
@@ -0,0 +1,5 @@
+module Jurnal
+  module Api
+    VERSION = "0.1.0"
+  end
+end
