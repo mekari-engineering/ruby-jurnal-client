4e2b6714151dd8a0768beb56070ab05d32d06b2c
Galih Muhammad <galih0muhammad@gmail.com>
2020-06-02 17:36:09 +0700

    Done reorganizing faraday connection

--- lib/jurnal_api/connection.rb
+++ lib/jurnal_api/connection.rb
@@ -16,22 +16,27 @@ module JurnalApi
       options = { headers: basic_headers, url: endpoint }.merge(connection_options)
 
       Faraday::Connection.new(options) do |connection|
-        case authorization_path
-        when 'partner/core'
-          connection.headers['Authorization'] = "Bearer #{access_token}"
-        when 'core'
-          connection.headers['apikey'] = "#{access_token}"
-        end
+        # SET middlewares first
+        connection.response   :logger
+        connection.use        FaradayMiddleware::RaiseHttpException
+        connection.use        Faraday::Request::UrlEncoded
 
-        connection.use Faraday::Request::UrlEncoded
         unless raw
           case format.to_s.downcase
             when 'json' then connection.use FaradayMiddleware::ParseJson
           end
         end
 
-        connection.use FaradayMiddleware::RaiseHttpException
-        connection.adapter(adapter)
+        # ...then the adapter
+        # this is to maintain the API of Faraday v1
+        connection.adapter adapter
+
+        case authorization_path
+        when 'partner/core'
+          connection.headers['Authorization'] = "Bearer #{access_token}"
+        when 'core'
+          connection.headers['apikey'] = "#{access_token}"
+        end        
       end
     end
   end
